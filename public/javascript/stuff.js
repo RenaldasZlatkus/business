298b1aa47659d751de87867360acde604fc9194c
Mai Berger <m.c.berger@icloud.com>
2015-10-23 14:39:23 +0200

    content added to order page, fixes #3

---
+++ public/javascript/stuff.js
@@ -0,0 +1,20 @@
+function setupPage(){
+	var outerWrapper = $("#lightbox-outer");
+	outerWrapper.hide();
+	
+	$(".second-wrapper").click(function(event) {
+		var link = $(event.currentTarget);
+		var imageLink = link.find('img').attr('src');
+		var image = $("#lightbox-image");
+
+		image.attr("src", imageLink);
+		outerWrapper.show();
+	});
+
+	outerWrapper.click(function(){
+		outerWrapper.hide();
+	});
+}
+
+$(document).ready(setupPage)
+
